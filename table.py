bae44222e7668df8da3a87cdd2af592fb48417d9
Elizaveta Tesleva <tesleva.elizaveta@yandex.ru>
2019-09-17 12:31:10 +0300

    init commit

---
+++ table.py
@@ -0,0 +1,14 @@
+num = int(input("Введите номер элемента:"))
+if num:
+    if num == 3:
+        print("Это Литий")
+    elif num == 17:
+        print("Это Хлор")
+    elif num == 25:
+        print("Это Марганец")
+    elif num == 80:
+        print("Это  Ртуть")
+    else:
+        print("Я не знаю что это")
+else:
+    print("Введите номер элемента!")
