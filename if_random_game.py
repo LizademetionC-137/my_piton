5e86a74109ba1ecfc42dd9b38fc3b993aba7b92a
Elizaveta Tesleva <tesleva.elizaveta@yandex.ru>
2019-09-19 13:14:13 +0300

    init commit

---
+++ if_random_game.py
@@ -0,0 +1,12 @@
+import random 
+random_num = random.randint(1,4) 
+a=int(input("Угадай число!: ")) 
+
+if a == random_num : 
+    print("Победа! Ты угадал!") 
+else : 
+    if ( a > random_num):
+        print("Я загадал число меньше твоего")
+    if ( a < random_num):
+        print("Я загадал число больше твоего")
+        
